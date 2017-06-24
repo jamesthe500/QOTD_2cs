9f55feb6809ff3770c2421090a2ba89cc1a9bffa
jamesthe500 <jamesddaniels@gmail.com>
2017-06-24 12:27:37 -0500

    Initial commit

---
+++ QOTD_2cs/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace QOTD_2cs
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
