a62bb628c6f52c9057d0201bf8b593fc374cbca1
Daniels <Daniels.James@mayo.edu>
2017-08-17 14:09:40 -0500

    Sets up basic functionality for an AnswersDB.

--- QOTD_2cs/Program.cs
+++ QOTD_2cs/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Data.Entity;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,23 +13,36 @@ namespace QOTD_2cs
     {
         public static void Main(string[] args)
         {
-            var answers = new List<Answer>
-            {
-                new Answer { Id = 0, Answerer = "Jim", Question = "Ever danced by the pale moon light?", Day = 0, AnswerText = "Not lately.", Index = 1},
-                new Answer { Id = 1, Answerer = "Jellybean", Question = "Ever danced by the pale moon light?", Day = 0, AnswerText = "Every chance I get!", Index = 1},
-                new Answer { Id = 2, Answerer = "Jim", Question = "What's your favorite admonishment?", Day = 1, AnswerText = "You should know better!", Index = 2},
-                new Answer { Id = 3, Answerer = "Jellybean", Question = "What's your favorite admonishment?", Day = 1, AnswerText = "I doth protest too much!", Index = 2},
-                new Answer { Id = 4, Answerer = "Jim", Question = "Who is your crush today?", Day = 2, AnswerText = "Jellybean.", Index = 3},
-                new Answer { Id = 5, Answerer = "Jellybean", Question = "Who is your crush today?", Day = 2, AnswerText = "Jim.", Index = 3}
-            };
+            var answers = ProcessAnswers("AnswersDB.csv");
             AddAnswer(answers);
             CountAnswers(answers);
         }
 
-        private static void AddAnswer(List<Answer> answerRepository)
+        private static List<Answer> ProcessAnswers(string path)
         {
-            answerRepository.Add(new Answer { AnswerText = "Wouldn't you like to know?"});
-            answerRepository.Add(new Answer { AnswerText = "123-34-4300" });
+            var query =
+                File.ReadAllLines(path)
+                    .Skip(1)
+                    .Where(l => l.Length > 1)
+                    .Select(l =>
+                        {
+                            var columns = l.Split(',');
+                            return new Answer
+                            {
+                                Answerer = columns[0],
+                                Question = columns[1],
+                                Day = int.Parse(columns[2]),
+                                AnswerText = columns[3],
+                                Index = int.Parse(columns[4]),
+                            };
+                        });
+            return query.ToList();
+        }
+
+        private static void AddAnswer(List<Answer> answers)
+        {
+            answers.Add(new Answer { AnswerText = "Wouldn't you like to know?", Index = 4 });
+            answers.Add(new Answer { AnswerText = "123-34-4300", Index = 4});
         }
 
         private static void CountAnswers(List<Answer> answerRepository)
@@ -43,20 +57,3 @@ namespace QOTD_2cs
     }
 }
 
-/*
-             * This block based on some instructions that require SQL Server. Not at my disposal just yet.
-             * it was in the main loop
-        // sets it up so that every time the app runs it is okay to drop and recreate the DB, simpler
-        Database.SetInitializer(new DropCreateDatabaseAlways<AnswersDb>());
-
-            // answerRepository allows us to work with the answer Db
-            // a SqlRepository requires a DbContext instance. Give enough info to create proper DbScheme
-            // wrapped in a using clause because it is disposable.
-            using (IRepository<Answer> answerRepository = new SqlRepository<Answer>(new AnswersDb()))
-            {
-                AddAnswer(answerRepository);
-                CountAnswers(answerRepository);
-                
-            }
-            //  using (IRepository<Answer> )
-            */
